dc5e41a4316382658c863ceaacc8eee507d4efcd
LulakshiHettiarachchi <lulakshinadunihettiarachchi@gmail.com>
2023-12-25 21:49:48 +0530

    insert data to database

--- src/main/java/com/example/sample_project/SampleProjectApplication.java
+++ src/main/java/com/example/sample_project/SampleProjectApplication.java
@@ -1,7 +1,9 @@
 package com.example.sample_project;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SampleProjectApplication {
@@ -9,5 +11,8 @@ public class SampleProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SampleProjectApplication.class, args);
 	}
-
+	@Bean
+	public ModelMapper ModelMapper(){
+		return new ModelMapper();
+	}
 }
---
+++ src/main/java/com/example/sample_project/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.sample_project.controller;
+
+import com.example.sample_project.dto.UserDTO;
+import com.example.sample_project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+@GetMapping("/getUser")
+    public String getUser(){
+    return "user Lulz";
+}
+
+@PostMapping("/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+
+        return userService.saveUser(userDTO);
+    }
+    @PutMapping("/updateUser")
+    public String updateUser(){
+        return "user Lulz updated";
+    }
+    @DeleteMapping("/deleteUser")
+    public String deletUser(){
+        return "user Lulz deleted";
+    }
+}
